e739d1dac89a39255daebd37435fed8fbea83b5e
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-02-02 12:54:07 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,52 +1,23 @@
-import os
+from tobrot.sample_config import Config
 
-class Config(object):
-    # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 12345))
-    # Get these values from my.telegram.org
-    # to store the channel ID who are authorized to use the bot
-    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "").split())
-    # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
-    # Telegram maximum file upload size
-    MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 2097152000
-    FREE_USER_MAX_FILE_SIZE = 50000000
-    # chunk size that should be used with requests
-    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
-    # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
-    # maximum message length in Telegram
-    MAX_MESSAGE_LENGTH = 4096
-    # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 3600
-    #
-    ARIA_TWO_STARTED_PORT = int(os.environ.get("ARIA_TWO_STARTED_PORT", 6800))
-    EDIT_SLEEP_TIME_OUT = int(os.environ.get("EDIT_SLEEP_TIME_OUT", 15))
-    MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = int(os.environ.get("MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START", 600))
-    MAX_TG_SPLIT_FILE_SIZE = int(os.environ.get("MAX_TG_SPLIT_FILE_SIZE", 1072864000))
-    # add config vars for the display progress
-    FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR", "█")
-    UN_FINISHED_PROGRESS_STR = os.environ.get("UN_FINISHED_PROGRESS_STR", "░")
-    # add offensive API
-    TG_OFFENSIVE_API = os.environ.get("TG_OFFENSIVE_API", None)
-    CUSTOM_FILE_NAME = os.environ.get("CUSTOM_FILE_NAME", "")
-    LEECH_COMMAND = os.environ.get("LEECH_COMMAND", "leech")
-    YTDL_COMMAND = os.environ.get("YTDL_COMMAND", "ytdl")
-    RCLONE_CONFIG = os.environ.get("RCLONE_CONFIG", "")
-    DESTINATION_FOLDER = os.environ.get("DESTINATION_FOLDER", "TorrentLeech-Gdrive")
-    GLEECH_COMMAND = os.environ.get("GLEECH_COMMAND", "gleech")
-    INDEX_LINK = os.environ.get("INDEX_LINK", "")
-    TELEGRAM_LEECH_COMMAND_G = os.environ.get("TELEGRAM_LEECH_COMMAND_G", "tleech")
-    CANCEL_COMMAND_G = os.environ.get("CANCEL_COMMAND_G", "cancel")
-    GET_SIZE_G = os.environ.get("GET_SIZE_G", "getsize")
-    STATUS_COMMAND = os.environ.get("STATUS_COMMAND", "status")
-    SAVE_THUMBNAIL = os.environ.get("SAVE_THUMBNAIL", "savethumbnail")
-    CLEAR_THUMBNAIL = os.environ.get("CLEAR_THUMBNAIL", "clearthumbnail")
-    UPLOAD_AS_DOC = os.environ.get("UPLOAD_AS_DOC", "False")
-    PYTDL_COMMAND_G = os.environ.get("PYTDL_COMMAND_G", "pytdl")
-    LOG_COMMAND = os.environ.get("LOG_COMMAND", "log")
+#read readme too before filling these stuffs
+
+class Config(Config):
+    TG_BOT_TOKEN= "1641612309:AAGPMdSG76CTmXo7QQD9S0DXTXeeq54foMA" #imp
+    APP_ID = 1309280 #imp
+    API_HASH = "af327dd857e0e65f80fefcf6d0af4afd" #imp
+    AUTH_CHANNEL = [-1001454182978, 1087968824] #imp replace your_id with your id from telegram or delete
+    INDEX_LINK = "https://bold-scene-480b.satyu.workers.dev"
+    GLEECH_COMMAND = "gleech@MyTorrentDrive_BOT"
+    YTDL_COMMAND = 'ytdl@MyTorrentDrive_BOT'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@MyTorrentDrive_BOT"
+    CLONE_COMMAND_G = "gclone@MyTorrentDrive_BOT"
+    PYTDL_COMMAND_G = "pytdl@MyTorrentDrive_BOT"
+    DESTINATION_FOLDER = "TorrentLeech-Gdrive"
+    LEECH_COMMAND = "leech@MyTorrentDrive_BOT"
+    RCLONE_CONFIG = """type = drive
+scope = drive
+root_folder_id =
+token = {"access_token":"ya29.A0AfH6SMCvaLMWW6iZacHYL43lGmR9QlSDfKXEFqCq52KTFmogEsc6EjFwcFIesczqQQNF52H9WVi1jGZgDWJF3R_19Rs-_hAcy7YeXIN-Tl3SNLB_dx4VmVPrGfvSJg3iACECKghtI4XTDBFZwtX8XD6esv9r","token_type":"Bearer","refresh_token":"1//0gdQV7KM1nnJrCgYIARAAGBASNwF-L9IralYTN1yMMrIcDDtrYLc4rBhVWENhbR-qJQxa1v0DzgfmUkYBtAW4PiZYoMqGx_IMObc","expiry":"2021-02-02T10:59:43.6364416+05:30"}
+team_drive = 0AKZow1MUSd9aUk9PVA """
+    #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""
