0b8ca142aa0d2ad9bfd3b1c2bd1a50e319594c31
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-02-02 23:03:51 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -17,7 +17,9 @@ class Config(Config):
 [DRIVE]
 type = drive
 scope = drive
-root_folder_id = 1nvU3oWuhh5tiyPUBz7AECQScv0Pvh9Qs
-token = {"access_token":"ya29.A0AfH6SMCvaLMWW6iZacHYL43lGmR9QlSDfKXEFqCq52KTFmogEsc6EjFwcFIesczqQQNF52H9WVi1jGZgDWJF3R_19Rs-_hAcy7YeXIN-Tl3SNLB_dx4VmVPrGfvSJg3iACECKghtI4XTDBFZwtX8XD6esv9r","token_type":"Bearer","refresh_token":"1//0gdQV7KM1nnJrCgYIARAAGBASNwF-L9IralYTN1yMMrIcDDtrYLc4rBhVWENhbR-qJQxa1v0DzgfmUkYBtAW4PiZYoMqGx_IMObc","expiry":"2021-02-02T10:59:43.6364416+05:30"}
+root_folder_id = 12qn5SetJIfHoynqKi2hZPyIZfNfyI5d-
+token = {"access_token":"ya29.a0AfH6SMAc6ULOacGMj_Mb23RM0UyvIukd3G0YRRPlIP4PPmon-HzDZQbRF2Rltd4N-jxIrf205sOnnqNRhXZ9_JsK1XeeRkEN8atiw4qAvYgcrEMChDAe28hozu5faCwZlJe0OVFqQ7rK-F41zGT2HqAjsHEKLeUoZ1IYtTgxAkA","token_type":"Bearer","refresh_token":"1//0gZPPb-gbNfvACgYIARAAGBASNwF-L9IrOFk3k6aawk9ScEyIOw8ow5nh8ZT2AFd0zHHykDpozCR60UF4qlEyQSPw2fDCTV22bd4","expiry":"2021-02-02T18:03:44.3770858+05:30"}
 team_drive = 0AKZow1MUSd9aUk9PVA
+client_id = 703529436612-guk8u72akf33ln7dvifdpcadtibvb62t.apps.googleusercontent.com
+client_secret = 0FoKBycxKRWeaDwlsuOUXAvR
 """
